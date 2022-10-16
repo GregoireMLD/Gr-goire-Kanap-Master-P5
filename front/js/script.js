9b91f32ef29a1c842eb7598f2b1b055eb82bd663
Grégoire Mailard <Gregoirepro@outlook.com>
2022-10-16 05:47:24 +0200

    first commit

---
+++ front/js/script.js
@@ -0,0 +1,19 @@
+fetch("http://localhost:3000/api/products")
+.then((response) => response.json())
+.then((data) => addProducts(data))
+
+function addProducts(produits) {
+    console.log(produits)
+    const imageUrl = produits[0].imageUrl
+    console.log("Url de l'image", imageUrl)
+    
+    const aremplacer = document.createElement("a")
+    aremplacer.href = imageUrl
+    aremplacer.text = "Un super Canap"
+    
+    const items = document.querySelector("#items")
+    if (items != null) {
+      items.appendChild(aremplacer) 
+      console.log("Nous avons bien le lien")
+    }  
+}
